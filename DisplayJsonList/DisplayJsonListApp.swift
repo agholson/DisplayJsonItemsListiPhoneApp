3a9de5a627df02b6d7f3644b3508a646dc241e31
agholson <87455899+agholson@users.noreply.github.com>
2023-02-25 09:55:03 -0500

    Initial Commit

---
+++ DisplayJsonList/DisplayJsonListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayJsonListApp.swift
+//  DisplayJsonList
+//
+//  Created by Leone on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct DisplayJsonListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
